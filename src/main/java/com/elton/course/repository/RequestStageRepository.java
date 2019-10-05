4f71671e9dd367bedc4473614b0828d5290c11f9
eltoncox <elton.unix@gmail.com>
2019-10-04 22:59:43 -0300

    update of RequestStageRepository

--- src/main/java/com/elton/course/repository/RequestStageRepository.java
+++ src/main/java/com/elton/course/repository/RequestStageRepository.java
@@ -1,5 +1,7 @@
 package com.elton.course.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.elton.course.domain.RequestStage;
 @Repository
 public interface RequestStageRepository extends JpaRepository<RequestStage, Long> {
 	
-	
+	public List<RequestStage> findAllByRequestId(Long id);
 
 }
